a56b06dc03d24e04332fe85c64a91a00d2416847
Andy Ung <andyung123456789@gmail.com>
2021-06-20 23:24:58 -0400

    new restrictive checks to website

---
+++ SEG3125-Module5-ValidatedService/main.js
@@ -0,0 +1,63 @@
+ function validateAll(){
+    const error = [10];
+    error[0] = validatePhone();
+    error[1] = validateEmail();
+    var ErrorMessage ="";
+
+    for(let i = 0; i < 2; i++){
+        if((error[i] == undefined) || (error[i] == null)){
+        }
+        else{
+            ErrorMessage += error[i] + ",\r"; 
+        }
+    }
+    document.getElementById('phone_error').innerHTML = ErrorMessage;
+}
+
+function validatePhoneNumber(input_str) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/im;
+
+    return re.test(input_str);
+}
+
+function validatePhone() {
+    var a = document.getElementById("phoneInput").value;
+
+    
+    // This filter asks for something like (12345), so parentheses with any number (at least 1)
+    // of digits
+
+    if(validatePhoneNumber(a)){
+    }else if(a == undefined){
+        return("Phone Number Blank");
+    }
+    else{
+        //document.getElementById("phone_error").innerHTML = "Phone Number Format Issue";
+        return("Phone Number Format Issue");
+    }
+    // if (validatePhoneNumber(validatePhoneNumber(a))) {
+    //     document.getElementById('phone_error').append("Phone Number Format correct");
+    // } else {
+    //     document.getElementById('phone_error').append("Phone Number Format Issue");
+    // }
+  }
+
+  function validateEmailAddress(input_str){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(input_str).toLowerCase());
+  }
+
+  function validateEmail() {
+    var a = document.getElementById("emailInput").value;
+
+    if(validateEmailAddress(a)){
+    }else{
+        //document.getElementById("phone_error").innerHTML = "Email Format Issue";
+        return("Email Format Issue");
+    }
+    // if (validatePhoneNumber(validatePhoneNumber(a))) {
+    //     document.getElementById('phone_error').append("Phone Number Format correct");
+    // } else {
+    //     document.getElementById('phone_error').append("Phone Number Format Issue");
+    // }
+  }
