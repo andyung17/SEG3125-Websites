066c5409235ce5a93847a893631455e5f9d94dda
Andy Ung <andyung123456789@gmail.com>
2021-06-20 23:55:21 -0400

    new restrictions

--- SEG3125-Module5-ValidatedService/main.js
+++ SEG3125-Module5-ValidatedService/main.js
@@ -1,10 +1,14 @@
  function validateAll(){
     const error = [10];
-    error[0] = validatePhone();
-    error[1] = validateEmail();
+    error[0] = validateName();
+    error[1] = validateGender();
+    error[2] = validatePhone();
+    error[3] = validateEmail();
+    error[4] = validateService();
+    error[5] = validateDoctor();
     var ErrorMessage ="";
 
-    for(let i = 0; i < 2; i++){
+    for(let i = 0; i < 10; i++){
         if((error[i] == undefined) || (error[i] == null)){
         }
         else{
@@ -60,4 +64,61 @@ function validatePhone() {
     // } else {
     //     document.getElementById('phone_error').append("Phone Number Format Issue");
     // }
+  }
+  
+  function validateFullName(input_str){
+    if((input_str == undefined) || (input_str == null) || (input_str == "")){
+        return "Empty Name";
+    }
+  }
+
+  function validateName() {
+    var a = document.getElementById("nameInput").value;
+    return(validateFullName(a));
+  }
+
+  function validateGender(){
+    var card = document.getElementById("genderSelect");
+    var selectedValue = card.options[card.selectedIndex].value;
+        if (selectedValue == "Male") {
+        }
+        else if(selectedValue == "Female"){
+        }
+        else if(selectedValue == "Other"){
+        }
+        else{
+            return "Missing Gender";
+        }
+  }
+
+  function validateService(){
+    var card = document.getElementById("serviceSelect");
+    var selectedValue = card.options[card.selectedIndex].value;
+        if (selectedValue == "Ophthalmic Surgery") {
+        }
+        else if(selectedValue == "Orthopaedic Surgery"){
+        }
+        else if(selectedValue == "Cardiology Surgery"){
+        }
+        else if(selectedValue == "Internal Organs Surgery"){
+        }
+        else if(selectedValue == "Oncology Surgery"){
+        }
+        else{
+            return "Missing Service";
+        }
+  }
+
+  function validateDoctor(){
+    var card = document.getElementById("doctorSelect");
+    var selectedValue = card.options[card.selectedIndex].value;
+        if (selectedValue == "Dr. Anne Limnae") {
+        }
+        else if(selectedValue == "Dr. Cameron Roy"){
+        }
+        else if(selectedValue == "Dr. Meghan Clark"){
+        }
+        else{
+            return "Missing Doctor";
+        }
   }
